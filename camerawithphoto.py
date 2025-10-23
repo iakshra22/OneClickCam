6bcccdbf409d25dcdd7365684c271d4704bd4401
Akshra <akshradang@gmail.com>
2025-10-23 21:41:31 +0530

    Update camerawithphoto.py

--- camerawithphoto.py
+++ camerawithphoto.py
@@ -1,7 +1,6 @@
 import cv2
 import datetime
 
-# Start capturing from default camera (0)
 cap = cv2.VideoCapture(0)
 
 while True:
@@ -15,7 +14,6 @@ while True:
     key_pressed = cv2.waitKey(1) & 0xFF
 
     if key_pressed == ord('s'):
-        # Generate a unique filename with timestamp
         timestamp = datetime.datetime.now().strftime("image_%Y%m%d_%H%M%S.jpg")
         cv2.imwrite(timestamp, frame)
         print(f"Image saved as {timestamp}")
@@ -24,7 +22,6 @@ while True:
     elif key_pressed == ord('q'):
         print("Exited without saving the image.")
         break
-
-# Release the webcam and destroy OpenCV windows
+        
 cap.release()
 cv2.destroyAllWindows()
